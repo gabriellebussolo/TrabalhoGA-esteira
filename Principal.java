eabf39f47cbff56c746e12b442d1c05bf39b1a86
gabriellebussolo <86787630+gabriellebussolo@users.noreply.github.com>
2022-10-07 19:20:43 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -98,7 +98,7 @@ public class Principal {
 			entrada_morango.join();
 			entrada_limao.join();
 			entrada_abacaxi.join();
-			Thread.sleep(6000);
+			Thread.sleep(7000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
